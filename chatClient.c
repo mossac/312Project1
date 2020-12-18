1661181275ca6520eef320315ee294c9b924af35
mossac <mossac@mossac-stg4.rose-hulman.edu>
2020-12-18 12:20:03 -0700

    final

--- chatClient.c
+++ chatClient.c
@@ -16,6 +16,7 @@
 char IP[] = "172.17.14.247";
 bool quit = false;
 char name[256];
+char othername[256];
 
 void main()
 {
@@ -25,15 +26,13 @@ void main()
     bzero(name, 256);
     fgets(name, 255, stdin);
     name[strlen(name) - 1] = '\0';
-    strcat(name, ": ");
+    
 
     // printf("name : %s \n",name);
     // n = write(sockfd, buffer, strlen(buffer));
     //printf("Your Name:%s", buffer);
-   
-            idk();
-        
-    
+
+    idk();
 }
 
 int idk(int argc, char *argv[])
@@ -64,53 +63,65 @@ int idk(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         herror("ERROR connecting");
+
+    n = write(sockfd, name, strlen(name));
+    if (n < 0)
+        error("ERROR writing to socket");
+    bzero(othername, 256);
+    if (read(sockfd, othername, 256) < 0)
+    {
+        error("ERROR reading from socket");
+    }
+   // printf("other name is: %s", othername);
+    //send my name
+
     int fresult = fork();
-    if(fresult < 0){
+    if (fresult < 0)
+    {
         printf("ERROR FORKING\n");
     }
-    if (fresult == 0){
+    if (fresult == 0)
+    {
         printf("Transmitting Side on\n");
-    while (1)
-    
-        
+        while (1)
+
         {
-       
-            
+
             printf("Please enter the message: ");
             bzero(buffer, 256);
             fgets(buffer, 255, stdin);
             //check to see if you can quit
-            if (strcmp(buffer, "quit\n") == 0)
+           
+          
+            //  printf("Buffer: %s\n", buffer);
+            n = write(sockfd, buffer, strlen(buffer));
+             if (strcmp(buffer, "quit\n") == 0)
             {
                 printf("QUIT!");
                 quit = true;
                 kill(0, SIGINT);
                 exit(1);
             }
-            char temp[strlen(name)];
-
-            strcpy(temp, name);
-            strcat(temp, buffer);
-            printf("Buffer: %s\n", buffer);
-            n = write(sockfd, temp, strlen(temp));
         }
     }
-    else  {
-        printf("Recieving Side on\n");
-        wait(1) ;
-       while (1)
+    else
+    {
+        char recieved[256];
+        for (;;)
         {
-            
+            bzero(recieved, 256);
+            n = read(sockfd, recieved, 255);
             if (n < 0)
-                herror("ERROR writing to socket");
+                error("ERROR reading from socket");
+            printf("%s\n", recieved);
+            fflush(stdout);
 
-            bzero(buffer, 256);
-            n = read(sockfd, buffer, 255);
-
-            if (n < 0)
-                herror("ERROR reading from socket");
-            printf("%s\n", buffer);
-            //fflush(stdout);
+            if (strcmp(recieved, "quit\n") == 0)
+            {
+                printf("\nUSER HAS DISCONNECTED!");
+                fflush(stdout);
+                kill(0, SIGINT);
+            }
         }
     }
 }
--- chatServer.c
+++ chatServer.c
@@ -2,12 +2,18 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
+#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
+#include <arpa/inet.h>
+
 #include <unistd.h>
 
 #define PORT 4567
+#define CLADDR_LEN 100
+char name[256];
+char othername[256];
+char clientAddr[CLADDR_LEN];
 
 void error(char *msg)
 {
@@ -21,6 +27,10 @@ int main(int argc, char *argv[])
     char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
     int n;
+    printf("Provide User Name: ");
+    bzero(name, 256);
+    fgets(name, 255, stdin);
+
     if (argc < 2)
     {
         fprintf(stderr, "ERROR, no port provided\n");
@@ -40,28 +50,70 @@ int main(int argc, char *argv[])
     listen(sockfd, 5);
     clilen = sizeof(cli_addr);
     newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-    int pid = fork();
-    if (pid ==1)
-    {
-    while(1)
-      if (newsockfd < 0)
+
+    if (newsockfd < 0)
         error("ERROR on accept");
-    bzero(buffer, 256);
-    n = read(newsockfd, buffer, 255);
-    if (n < 0)
+
+    //get other name
+    bzero(othername, 256);
+    if (read(newsockfd, othername, 256) < 0)
+    {
         error("ERROR reading from socket");
-    printf(" %s\n", buffer);
-    n = write(newsockfd, "I got your message", 18);
+    }
+    printf("other name is: %s", othername);
+    //send my name
+    n = write(newsockfd, name, strlen(name));
     if (n < 0)
         error("ERROR writing to socket");
-    
+
+    inet_ntop(AF_INET, &(cli_addr.sin_addr), clientAddr, CLADDR_LEN);
+    printf("Connection accepted from %s...\n", clientAddr);
+
+    int pid = fork();
+    if (pid < 0)
+    {
+        printf("ERROR FORKING\n");
     }
-    if( pid==0){
+    if (pid == 0)
+    {
+        while (1)
+        {
+            printf("Please enter the message: ");
+            bzero(buffer, 256);
+             //printf("stdin\n");
+            fgets(buffer, 255, stdin);
+            //check to see if you can quit
 
-
-
-
+            //printf("Buffer: %s , %d\n", buffer, newsockfd);
+            n = write(newsockfd, buffer, strlen(buffer));
+            //printf("printf, %d\n", sockfd);
+            if (strcmp(buffer, "quit\n") == 0)
+            {
+                printf("QUIT!");
+            
+                kill(0, SIGINT);
+                exit(1);
+            }
+        }
     }
-    
-    
+
+    else
+    {
+        char recieved[256];
+        for (;;)
+        {
+            bzero(recieved, 256);
+            n = read(newsockfd, recieved, 256);
+            if (n < 0)
+                error("ERROR reading from socket");
+
+            printf("\n<%s> %s\n", othername, recieved);
+            fflush(stdout);
+            if (strcmp(recieved, "quit\n") == 0)
+            {
+                printf("\nUSER HAS DISCONNECTED!");
+                fflush(stdout);
+                kill(0, SIGINT);
+            }
+    }
 }
